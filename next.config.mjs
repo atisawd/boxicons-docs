9267aedd05f6c561aa0df0aa62667ad253d6784f
atisawd <40288907+atisawd@users.noreply.github.com>
2025-05-07 17:11:50 +0530

    docs publish

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,14 @@ const withNextra = nextra({
   // ... Add Nextra-specific options here
   latex: true,
   codeHighlight:true,
-  search: { codeblocks: false },
-  output: 'export',
-  basePath: "/boxicons-docs"
+  search: { codeblocks: false }
+
 })
  
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
-  reactStrictMode: true
+  reactStrictMode: true,
+    output: 'export',
+  basePath: "/boxicons-docs"
 })
