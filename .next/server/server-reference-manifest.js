820890029061e0b267b1af6878267d819327be71
atisawd <40288907+atisawd@users.noreply.github.com>
2025-05-07 16:39:35 +0530

    boxicons-docs

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yVvGtZzC+ktlyvTxWCwgWjZIlfnNwuDd6ncsniT/Q8Y=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
